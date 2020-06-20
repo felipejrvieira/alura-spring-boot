1574aa8a65d82c82c9b8190f45d262e4c948425f
Felipe J. R. Vieira <felipejrvieira@gmail.com>
2020-06-20 11:13:06 -0300

    Simplificando a utilizacao de paginacao e ordenacao.

--- src/main/java/br/com/alura/forum/controller/TopicosController.java
+++ src/main/java/br/com/alura/forum/controller/TopicosController.java
@@ -1,7 +1,6 @@
 package br.com.alura.forum.controller;
 
 import java.net.URI;
-import java.util.List;
 import java.util.Optional;
 
 import javax.transaction.Transactional;
@@ -9,8 +8,9 @@ import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort.Direction;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -41,13 +41,10 @@ public class TopicosController {
     private CursoRepository cursoRepository;
 
     @GetMapping
-    public Page<TopicoDto> lista(@RequestParam(required = false) String nomeCurso, @RequestParam int pagina,
-            @RequestParam int qtd) {
-
-        Pageable paginacao = PageRequest.of(pagina, qtd);
-
+    public Page<TopicoDto> lista(@RequestParam(required = false) String nomeCurso,
+            @PageableDefault(sort = "id", direction = Direction.ASC) Pageable paginacao) {
         if (nomeCurso != null) {
-            Page<Topico> topicos = topicoRepository.findByCursoNome(nomeCurso,paginacao);
+            Page<Topico> topicos = topicoRepository.findByCursoNome(nomeCurso, paginacao);
             return TopicoDto.converter(topicos);
         } else {
             Page<Topico> topicos = topicoRepository.findAll(paginacao);
