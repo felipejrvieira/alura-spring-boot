91a4716c7a1620f42e8aefff115e1af9d7ff3ab7
Felipe J. R. Vieira <felipejrvieira@gmail.com>
2020-06-20 14:40:57 -0300

    Inclusão de segurança no acesso a api.

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,4 +1,4 @@
-INSERT INTO USUARIO(nome, email, senha) VALUES('Aluno', 'aluno@email.com', '123456');
+INSERT INTO USUARIO(nome, email, senha) VALUES('Aluno', 'aluno@email.com', '$2a$10$YsXeiUFJv9KwAHrLv8IlE.IzqIIrxeJQ5v.Gdcv0WX5LpgLvhm3ZW');
 
 INSERT INTO CURSO(nome, categoria) VALUES('Spring Boot', 'Programação');
 INSERT INTO CURSO(nome, categoria) VALUES('HTML 5', 'Front-end');
