7f0a03c26e9560c2bbc52be4d55e6dc702f23772
Felipe J. R. Vieira <felipejrvieira@gmail.com>
2020-06-20 17:34:29 -0300

    Validando o JWT para realização de operações com autenticação.

--- src/main/java/br/com/alura/forum/controller/AutenticaoController.java
+++ src/main/java/br/com/alura/forum/controller/AutenticaoController.java
@@ -11,7 +11,6 @@ import org.springframework.security.core.AuthenticationException;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.alura.forum.config.security.TokenService;
